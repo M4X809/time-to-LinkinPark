69c7bdc12b9e36d351ccf256fa83149903229de7
m4x809 <github@j-pet.de>
2025-05-10 13:51:12 +0200

    Init

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+function App() {
+	const [count, setCount] = useState(0);
+
+	return (
+		<>
+			<h1 className="text-4xl font-bold">Time to Linkin Park</h1>
+			<div className="card">
+				<p>
+					Edit <code>src/App.tsx</code> and save to test HMR
+				</p>
+			</div>
+			<p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+		</>
+	);
+}
+
+export default App;
