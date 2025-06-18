530e18df5d61c0bd9214b9f422627c98dc0666a5
M4X809 <github@j-pet.de>
2025-06-18 19:23:43 +0200

    feat: update to new concert

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import { Countdown } from "./components/Countdown";
 
 function App() {
-	const concertDate = new Date("2025-06-16T20:50:00");
-	const albumDate = new Date("2025-05-16T00:00:00");
+	const concertDate = new Date("2026-06-03T18:30:00");
 	return (
 		<div className="flex h-screen flex-col items-center justify-center text-white">
 			<main className="flex flex-col items-center justify-center gap-4">
 				<Countdown targetDate={concertDate} title="Time to Linkin Park Concert" />
-				{/* <Countdown targetDate={albumDate} title="Time to Linkin Park Album" /> */}
 			</main>
 		</div>
 	);
