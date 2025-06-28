fd0eafb4d0d8456fffed151788396a689ff3fc8e
m4x809 <github@j-pet.de>
2025-06-28 22:31:37 +0200

    chore: update dependencies in package.json and bun.lock, add VitePWA plugin to vite.config.ts for improved PWA support

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig(() => {
 	return {
 		plugins: [
+			VitePWA({
+				registerType: "autoUpdate",
+				workbox: {
+					maximumFileSizeToCacheInBytes: 10 * 1024 * 1024, // 10MB
+				},
+			}),
 			react({
 				babel: {
 					plugins: [["babel-plugin-react-compiler"]],
